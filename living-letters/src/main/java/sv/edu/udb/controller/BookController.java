18902f76c6b172d1138605371eda26ed2e6a7236
Keileb-boop <125236416+Keileb-boop@users.noreply.github.com>
2025-08-02 00:30:31 -0600

    Method PUT for UPDATE a book
    Method update 3

--- living-letters/src/main/java/sv/edu/udb/controller/BookController.java
+++ living-letters/src/main/java/sv/edu/udb/controller/BookController.java
@@ -43,4 +43,15 @@ public class BookController {
     public void deleteBook(@PathVariable Long id) {
         bookService.deleteBook(id);
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Book> updateBook(@PathVariable Long id, @RequestBody Book updatedBook) {
+        Book book = bookService.updateBook(id, updatedBook);
+            if (book != null) {
+        return ResponseEntity.ok(book);
+            } else {
+        return ResponseEntity.notFound().build();
+    }
+}
+    
 }
